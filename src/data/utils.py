39bcca74c44af023a5bce63cca90fae95fbaa5d5
HassanRady <hassan.khaled.rady@gmail.com>
2025-02-01 10:09:41 +0100

    Update

--- src/data/utils.py
+++ src/data/utils.py
@@ -31,62 +31,6 @@ def bin_data(train, test, columns, n_bins=10):
     return train, test
 
 
-# class Impute_With_Model(BaseEstimator, TransformerMixin):
-#     def __init__(self, model, na_frac=0.5, min_samples=0):
-#         self.model = model
-#         self.na_frac = na_frac
-#         self.min_samples = min_samples
-#         self.model_dict = {}
-#         self.mean_dict = {}
-#         self.features = None
-#
-#     def find_features(self, data, feature, tmp_features):
-#         missing_rows = data[feature].isna()
-#         na_fraction = data[missing_rows][tmp_features].isna().mean(axis=0)
-#         valid_features = np.array(tmp_features)[na_fraction <= self.na_frac]
-#         return valid_features
-#
-#     def fit(self, X, y=None):
-#         self.features = X.columns.tolist()
-#         n_data = X.shape[0]
-#         for feature in self.features:
-#             self.mean_dict[feature] = np.mean(X[feature])
-#         for feature in tqdm(self.features):
-#             if X[feature].isna().sum() > 0:
-#                 model_clone = clone(self.model)
-#                 X_feature = X[X[feature].notna()].copy()
-#                 tmp_features = [f for f in self.features if f != feature]
-#                 tmp_features = self.find_features(X, feature, tmp_features)
-#                 if len(tmp_features) >= 1 and X_feature.shape[0] > self.min_samples:
-#                     for f in tmp_features:
-#                         X_feature[f] = X_feature[f].fillna(self.mean_dict[f])
-#                     model_clone.fit(X_feature[tmp_features], X_feature[feature])
-#                     self.model_dict[feature] = (model_clone, tmp_features.copy())
-#                 else:
-#                     self.model_dict[feature] = ("mean", np.mean(X[feature]))
-#         return self
-#
-#     def transform(self, X):
-#         imputed_data = X.copy()
-#         for feature, model in self.model_dict.items():
-#             missing_rows = imputed_data[feature].isna()
-#             if missing_rows.any():
-#                 if model[0] == "mean":
-#                     imputed_data[feature].fillna(model[1], inplace=True)
-#                 else:
-#                     tmp_features = model[1]
-#                     X_missing = X.loc[missing_rows, tmp_features].copy()
-#                     for f in tmp_features:
-#                         X_missing[f] = X_missing[f].fillna(self.mean_dict[f])
-#                     imputed_data.loc[missing_rows, feature] = model[0].predict(
-#                         X_missing[tmp_features]
-#                     )
-#         return imputed_data
-#
-#     def __str__(self):
-#         return "Impute_With_Model"
-
-
 class Impute_With_Model(BaseEstimator, TransformerMixin):
     def __init__(self, model, na_frac=0.5, min_samples=0):
         self.model = model
